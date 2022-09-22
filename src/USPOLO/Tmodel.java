9cd39bfbb23c58b82e1b6150518e2439437a536b
Nikhil Venket <nikhil.venket@provana.com>
2022-09-22 16:45:49 +0530

    Loading Concept

--- src/USPOLO/Tmodel.java
+++ src/USPOLO/Tmodel.java
@@ -1,18 +1,18 @@
 package USPOLO;
-//class contains variables,methods,main method,object
+
 public class Tmodel extends Tshirt {
     int Count;
     String Colour;
     char Size;
-// Static method is first exection 
-// we declare public static can we call the method as global
-//static block execution stack from when we run the program    
-    static {
+
+    static
+    {
         System.out.println("USpolo-Tshirt");
     }
 
-    Tmodel(int Count, String Colour, char Size) {
-        this.Count = Count;
+    Tmodel(int i, String Colour, char Size)
+    {
+        this.Count = i;
         this.Colour = Colour;
         this.Size = Size;
         System.out.println("USPOLO T-shirt");
@@ -34,8 +34,6 @@ public class Tmodel extends Tshirt {
     }
 
     public static void main(String[] args) {
-        
-        //object gives memory allocation
         Tmodel mod=new Tmodel(1000,"White", 'L');
         mod.User1();
         mod.User2();
